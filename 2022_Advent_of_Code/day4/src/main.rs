9aed611770f613c361d2e0fe3816804016df1c95
Luna Astris <84162680+LunaAstris16@users.noreply.github.com>
2022-12-05 10:18:07 -0500

    Update main.rs

--- 2022_Advent_of_Code/day4/src/main.rs
+++ 2022_Advent_of_Code/day4/src/main.rs
@@ -2,14 +2,76 @@ use std::fs::File;
 use std::io::Read;
 
 fn main() {
-    let filename = "testingday4.txt";
+    let filename = "inputday4.txt";
     let file_data = lines_from_file(filename);
-      
+    let mut total = 0;
+  
     for x in file_data {
-        let temp: Vec<&str> = x.split(",").collect();
-        let number1: Vec<i32> = (temp[0][0..1]..temp[0][2..3]).collect();
-        println!("{:?}", number1);
+      let temp: Vec<&str> = x.split(",").collect();
+      let mut nums: Vec<i32> = Vec::new();
+      let num1: Vec<&str> = temp[0].split("-").collect();
+      let num2: Vec<&str> = temp[1].split("-").collect();
+      nums.push(num1[0].parse::<i32>().unwrap());
+      nums.push(num1[1].parse::<i32>().unwrap());
+      nums.push(num2[0].parse::<i32>().unwrap());
+      nums.push(num2[1].parse::<i32>().unwrap());
+      let number1: Vec<i32> = (nums[0]..nums[1]+1).collect();
+      let number2: Vec<i32> = (nums[2]..nums[3]+1).collect();
+      if number1.len() <= number2.len() {
+        if number1.iter().all(|e| number2.contains(e)) == true{
+          total = total + 1;
+        }
+      } else {
+        if number2.iter().all(|e| number1.contains(e)) == true{
+          total = total + 1;
+        }
+      }
     }
+  
+  println!("{}", total);
+
+  //part 2
+    total = 0;
+    let filename = "inputday4.txt";
+    let file_data = lines_from_file(filename);
+  
+    for x in file_data {
+      let temp: Vec<&str> = x.split(",").collect();
+      let mut nums: Vec<i32> = Vec::new();
+      let num1: Vec<&str> = temp[0].split("-").collect();
+      let num2: Vec<&str> = temp[1].split("-").collect();
+      nums.push(num1[0].parse::<i32>().unwrap());
+      nums.push(num1[1].parse::<i32>().unwrap());
+      nums.push(num2[0].parse::<i32>().unwrap());
+      nums.push(num2[1].parse::<i32>().unwrap());
+      let number1: Vec<i32> = (nums[0]..nums[1]+1).collect();
+      let number2: Vec<i32> = (nums[2]..nums[3]+1).collect();
+      if number1.len() <= number2.len() {
+        let mut i = 0;
+        while i < number1.len() {
+          if number2.contains(&number1[i]) == true{
+            total = total + 1;
+            i = number1.len()+1;
+          }
+          else {
+            i = i + 1;
+          }
+        }
+      } else {
+        let mut i = 0;
+        while i < number2.len() {
+          if number1.contains(&number2[i]) == true{
+            total = total + 1;
+            i = number2.len()+1;
+          }
+          else {
+            i = i + 1;
+          }
+        }
+      }
+    }
+  
+    println!("{}", total);
 }
 
 fn lines_from_file(filename: &str) -> Vec<String> {
