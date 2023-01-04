891f9804b33b1db04a9de77780ec337630001b6e
Luna Astris <lunaastris15@gmail.com>
2023-01-04 10:41:47 -0500

    Added mire days

---
+++ 2022_Advent_of_Code/Day1/target/debug/Day1.d
@@ -0,0 +1,1 @@
+E:\Advent\ of\ code\ 2022\Advent-of-Code-2022\2022_Advent_of_Code\Day1\target\debug\Day1.exe: E:\Advent\ of\ code\ 2022\Advent-of-Code-2022\2022_Advent_of_Code\Day1\src\main.rs
---
+++ 2022_Advent_of_Code/Day1/target/debug/deps/Day1.d
@@ -0,0 +1,5 @@
+E:\Advent of code 2022\Advent-of-Code-2022\2022_Advent_of_Code\Day1\target\debug\deps\Day1.exe: src\main.rs
+
+E:\Advent of code 2022\Advent-of-Code-2022\2022_Advent_of_Code\Day1\target\debug\deps\Day1.d: src\main.rs
+
+src\main.rs:
